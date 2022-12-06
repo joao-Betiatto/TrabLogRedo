ac80e46137a5927606c8104502080c5b1a4f2b45
joao-Betiatto <85709175+joao-Betiatto@users.noreply.github.com>
2022-12-06 10:32:51 -0300

    testando para abrir o arquivo txt

--- LogRedo.py
+++ LogRedo.py
@@ -6,6 +6,16 @@ def conecta_bd():                 #criando conexão com o banco
         host='localhost', 
         database='TrabLog',
         user='postgres', 
-        password='12345')
+        password='Joaointer23')
 
     return connect
+
+#teste abrindo arquivo
+fileName='entrada.txt'
+try:
+    file=open(fileName, "r", encoding="utf-8")
+except:
+    print('Não foi possível abrir o arquivo')
+    exit(0)
+
+fileArray=file.read().splitlines()
